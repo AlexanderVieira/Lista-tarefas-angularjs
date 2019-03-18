2077daf2f7b285cc5d86f963e33c5e7fb5d1b234
Alexander <asilva943rj@gmail.com>
2019-03-18 16:46:34 -0300

    Alterações no styles.css, taskService, navigation-component e index

--- app/services/taskService.js
+++ app/services/taskService.js
@@ -1,37 +1,62 @@
+// (function () {
+//
+//     angular.module('taskApp', [])
+//         .factory('taskService', ['$http', '$q', '$log', taskService]);
+//
+//     function taskService($http, $q, $log) {
+//
+//         return {
+//             getAllTasks: getAllTasks,
+//             getAllCategories: getAllCategories
+//         };
+//
+//         function getAllTasks() {
+//             return $http.get('data/tasks.json')
+//                 .then(function(response) {
+//                     return response.data;
+//                 })
+//                 .catch(function(response) {
+//                     $log.error('Error retrieving tasks: ' + response.statusText);
+//                     return $q.reject('Error retrieving tasks.');
+//                 })
+//         }
+//
+//         function getAllCategories() {
+//             return $http.get('data/categories.json')
+//                 .then(function(response) {
+//                     return response.data;
+//                 })
+//                 .catch(function(response) {
+//                     $log.error('Error retrieving categories: ' + response.statusText);
+//                     return $q.reject('Error retrieving categories.');
+//                 })
+//         }
+//
+//     }
+//
+// }());
+
 (function () {
+    'use strict';
 
-    angular.module('taskApp', [])
-        .factory('taskService', ['$http', '$q', '$log', taskService]);
+    angular.module('taskApp').factory('taskService', function ($http) {
 
-    function taskService($http, $q, $log) {
+        var self = this;
 
-        return {
-            getAllTasks: getAllTasks,
-            getAllCategories: getAllCategories
+        self.getAllTasks = function () {
+            return $http.get('data/tasks.json')
+                .then(function (result) {
+                    return result.data;
+                });
         };
 
-        function getAllTasks() {
-            return $http.get('data/tasks.json')
-                .then(function(response) {
-                    return response.data;
-                })
-                .catch(function(response) {
-                    $log.error('Error retrieving tasks: ' + response.statusText);
-                    return $q.reject('Error retrieving tasks.');
-                })
-        }
-
-        function getAllCategories() {
+        self.getAllCategories = function () {
             return $http.get('data/categories.json')
-                .then(function(response) {
-                    return response.data;
-                })
-                .catch(function(response) {
-                    $log.error('Error retrieving categories: ' + response.statusText);
-                    return $q.reject('Error retrieving categories.');
-                })
-        }
+                .then(function (result) {
+                    return result.data;
+                });
+        };
 
-    }
-
-}());
+        return this;
+    });
+})();
